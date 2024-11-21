a6b89258e19f768afe348dfd72cf1225b6747c2d
clemencY-J <jjjj04444@handong.ac.kr>
2024-11-21 10:31:49 +0900

    netli

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import Componentc from './components/Componentc.js';
-import Componentf from './components/Componentf.js';
-import Hello from './components/Hello.js';
-import C20241111 from './components/C20241111.js';
-import C20241114a from './components/C20241114a.js';
 import Router from './components/Router.js'
-import C20241118a from './components/C20241118a.js';
-import C20241118UseStateA from './components/C20241118UseStateA.js';
-import C20241118UseStateB from './components/C20241118UseStateB.js';
-import C20241118UseStateC from './components/C20241118UseStateC.js';
-import C20241118UseStateD from './components/C20241118UseStateD.js';
-import C20241121 from './components/C20241121.js';
 
 function App() {
 
